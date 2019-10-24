7f0a8bc264ddc8b6f157cffa0ba0c43a4ffd03a2
Marcos Castelo <marcos.castelo@bpf.com.br>
2019-10-24 10:42:33 -0300

    searchEnterTrigger function implemented

--- src/main.js
+++ src/main.js
@@ -1,20 +1,13 @@
 import spotify from './spotify';
-import renderAlbums from './albumList';
 import renderAlbumInfo from './albumInfo';
 import renderAlbumTracks from './albumTracks';
+import searchEnterTrigger from './searchTrigger';
 
-const albums = spotify.search.albums('Guns n Roses');
-const albumList = document.getElementById('album-list');
+searchEnterTrigger();
+
 const album = spotify.album.getAlbum('3I9Z1nDCL4E0cP62flcbI5');
 const albumInfo = document.getElementById('album-info');
 const albumTracks = document.getElementById('album-tracks');
-
-
-albums
-  .then((data) => {
-    renderAlbums(data.albums.items, albumList);
-    console.log(data);
-  });
 
 album
   .then((data) => renderAlbumInfo(data, albumInfo))
