d68a18f1076cdd0dada0900aa64f478f5de7de33
Marcos Castelo <marcos.castelo@bpf.com.br>
2019-10-25 16:03:08 -0300

    Main Methods finalized

--- src/main.js
+++ src/main.js
@@ -1,14 +1,7 @@
-import spotify from './spotify';
-import renderAlbumInfo from './albumInfo';
-import renderAlbumTracks from './albumTracks';
 import searchEnterTrigger from './searchTrigger';
+import selectAlbumTrigger from './selectAlbumTrigger';
+import playlistTrigger from './playlistTrigger';
 
 searchEnterTrigger();
-
-const album = spotify.album.getAlbum('3I9Z1nDCL4E0cP62flcbI5');
-const albumInfo = document.getElementById('album-info');
-const albumTracks = document.getElementById('album-tracks');
-
-album
-  .then((data) => renderAlbumInfo(data, albumInfo))
-  .then((data) => renderAlbumTracks(data.tracks.items, albumTracks));
+selectAlbumTrigger();
+playlistTrigger();
--- src/spotify.js
+++ src/spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'ma-spotify-wrapper/lib/index';
 
 const spotify = new SpotifyWrapper({
-  token: 'BQBCgNg3w5W1O_TDNy4mPECfhFhVvUX5od1n2jt4JsM5si_uNTeEVbfsimrluY6bvnl7X9VYPeI0cyTHu0jrXKfyro5oMmFGRzv6_aV-zDxNYsNuW3REJ9Yj060n_98H4AQYrl80V9-ZpIeM5aS9dxQnyePi_BjSKs8fYtBlNsOCWYQl1LAsmGtzCr6dJ_mymmIM93HZlJ7qXFbox8uxB33EGfOpy_40wx7_K5gvsPcns_mwiQExw_muUh0smqT8v9tB_IuPpS4',
+  token: 'BQApl2MTsVAgoXvvnTR2T85K7qMY9oWUWmxicdtV9Rk-Fw2t_ka6tqPG-cGD4XnTG3-nF6mgy59gVIOIJfqjC7CrICM4aFzgDzo5i2ynR4OlgdeGwbmRhLkh2yeO4CtxRtm-8SDmf1CPEyPrH-sOFktaxi9wBPUyzoewcRzfQTi-3thT_b8ZWToz42ysOBJ_3pZRfbLKEijQDHidbQp2yLv9Kj2xPFXCbDa6K0IJnFNevVFoESuvi5R-e8X1GTgWnaQml9jES58',
 });
 
 export default spotify;
